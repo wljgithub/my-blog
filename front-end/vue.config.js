b4c8cbe66961439d1b0a60aa1f9317e4622c0946
jack <xiulu.tech@outlook.com>
2020-04-07 12:48:57 +0800

    build at gzip format in all enviroment

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -2,18 +2,16 @@ const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   configureWebpack: (config) => {
-    if (process.env.NODE_ENV === "development") {
-      return {
-        plugins: [
-          new CompressionPlugin({
-            algorithm: "gzip",
-            test: /\.(js|css)$/, // patter name
-            threshold: 10240, // compress file that sizes large than 10k
-            deleteOriginalAssets: true, // delete source
-            minRatio: 0.8, // compress ratio
-          }),
-        ],
-      };
-    }
+    return {
+      plugins: [
+        new CompressionPlugin({
+          algorithm: "gzip",
+          test: /\.(js|css)$/, // patter name
+          threshold: 10240, // compress file that sizes large than 10k
+          deleteOriginalAssets: true, // delete source
+          minRatio: 0.8, // compress ratio
+        }),
+      ],
+    };
   },
 };
