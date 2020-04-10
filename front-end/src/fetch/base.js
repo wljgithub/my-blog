2ffed9c1dba997b141ad867c387fe785338084ff
jack <xiulu.tech@outlook.com>
2020-04-11 00:27:40 +0800

    add production condition to build file

--- front-end/src/fetch/base.js
+++ front-end/src/fetch/base.js
@@ -2,17 +2,20 @@ import axios from "axios";
 import token from "@/utils/token";
 
 const prefix = "/api";
-axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+if (process.env.NODE_ENV != "production") {
+  axios.defaults.baseURL = process.env.VUE_APP_LOCAL_API_URL;
+}
+
 axios.defaults.timeout = 30000;
 axios.interceptors.request.use(
-  function (config) {
+  function(config) {
     // carried token if exist in local storage
     if (token.checkToken()) {
       config.headers[token.tokenName] = token.getToken();
     }
     return config;
   },
-  function (error) {
+  function(error) {
     return Promise.reject(error);
   }
 );
@@ -28,13 +31,13 @@ export default (url = "", params = {}, method = "get") => {
     method: method,
     headers: headers,
     data: params,
-    url,
+    url
   };
   return new Promise((resolve, reject) => {
     axios(options)
-      .then((res) => {
+      .then(res => {
         resolve(res.data);
       })
-      .catch((err) => reject(err.data));
+      .catch(err => reject(err.data));
   });
 };
--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,17 +1,19 @@
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-  configureWebpack: (config) => {
-    return {
-      plugins: [
-        new CompressionPlugin({
-          algorithm: "gzip",
-          test: /\.(js|css)$/, // patter name
-          threshold: 10240, // compress file that sizes large than 10k
-          deleteOriginalAssets: true, // delete source
-          minRatio: 0.8, // compress ratio
-        }),
-      ],
-    };
-  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            algorithm: "gzip",
+            test: /\.(js|css)$/, // patter name
+            threshold: 10240, // compress file that sizes large than 10k
+            deleteOriginalAssets: true, // delete source
+            minRatio: 0.8 // compress ratio
+          })
+        ]
+      };
+    }
+  }
 };
