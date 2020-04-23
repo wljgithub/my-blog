b33768ee6e5499f082453644963730af372d7685
jack <xiulu.tech@outlook.com>
2020-04-23 23:45:31 +0800

    add catch router fall-back to nginx

--- manage.sh
+++ manage.sh
@@ -332,7 +332,7 @@ http {
           server_name xiulu.xyz;
           location / {
                 root /data/www;
-                try_files $uri $uri/ /index.html;
+                try_files \$uri \$uri/ /index.html;
         }
           location /api {
 
